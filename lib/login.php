de17703d6c101e3dddac438c37002e7bbd2b56dd
Mika Lindqvist <postmaster@raasu.org>
2019-10-04 01:59:26 +0200

    When logging in with e-mail address, don't reveal spend key until auth code is verified.

--- lib/login.php
+++ lib/login.php
@@ -19,24 +19,18 @@ function login_no_auth($spendKey, $email) {
     exit();
   }
 }
-// Check if user has logged in or not?
-if (isset($_POST['spendKey']) && !isset($_POST['authCode'])) {
-  // User is logging in...
-  $spendKey = $_POST['spendKey'];
-  if (!validate_spendkey($spendKey)) {
-    echo "<span class='error'>Invalid spend key!</span></div></body></html>";
+// This version avoids leaking spend key
+function login_no_auth_with_email($spendKey, $email) {
+  $authCode = generate_authcode($spendKey);
+  if (strlen($email) > 0) {
+    send_login_email($email, $authCode);
+    echo "<form action='index.php' method='post'>";
+    echo "<input type='hidden' name='email' value='", $email, "'>";
+    echo "Authentication code: <input type='number' pattern='[0-9]{6}' name='authCode' placeholder='000000' required size='6'><br>";
+    echo "<input type='submit' name='Verify' class='btn' value='Verify'>";
+    echo "</form></body></html>";
     exit();
   }
-  $email = get_email_with_spendkey($spendKey);
-  login_no_auth($spendKey, $email);
-}
-if (isset($_POST['spendKey']) && isset($_POST['authCode'])) {
-  $spendKey = $_POST['spendKey'];
-  if (!validate_spendkey($spendKey)) {
-    echo "<span class='error'>Invalid spend key!</span></div></body></html>";
-    exit();
-  }
-  $authCode = $_POST['authCode'];
   if (log_in($spendKey, $authCode)) {
     echo "Wallet ", get_address($spendKey), " logged in!<br>";
     echo "<script>document.location='index.php';</script>";
@@ -45,7 +39,31 @@ if (isset($_POST['spendKey']) && isset($_POST['authCode'])) {
     exit();
   }
 }
-if (isset($_POST['email']) && !isset($_POST['authCode'])) {
+// Check if user has logged in or not?
+// Using spend key?
+if (isset($_POST['spendKey'])) {
+  $spendKey = $_POST['spendKey'];
+  if (!validate_spendkey($spendKey)) {
+    echo "<span class='error'>Invalid spend key!</span></div></body></html>";
+    exit();
+  }
+  if (isset($_POST['authCode'])) {
+    $authCode = $_POST['authCode'];
+    if (log_in($spendKey, $authCode)) {
+      echo "Wallet ", get_address($spendKey), " logged in!<br>";
+      echo "<script>document.location='index.php';</script>";
+    } else {
+      echo "<span class='error'>Authentication code not verified!</span></div></body></html>";
+      exit();
+    }
+  } else {
+    // User is logging in...
+    $email = get_email_with_spendkey($spendKey);
+    login_no_auth($spendKey, $email);
+  }
+}
+// Using email?
+if (isset($_POST['email'])) {
   $email = $_POST['email'];
   if (!validate_email($email)) {
     echo "<span class='error'>Invalid e-mail address!</span></div></body></html>";
@@ -56,8 +74,20 @@ if (isset($_POST['email']) && !isset($_POST['authCode'])) {
     echo "<span class='error'>Invalid spend key!</span></div></body></html>";
     exit();
   }
-  login_no_auth($spendKey, $email);
+  if (isset($_POST['authCode'])) {
+    $authCode = $_POST['authCode'];
+    if (log_in($spendKey, $authCode)) {
+      echo "Wallet ", get_address($spendKey), " logged in!<br>";
+      echo "<script>document.location='index.php';</script>";
+    } else {
+      echo "<span class='error'>Authentication code not verified!</span></div></body></html>";
+      exit();
+    }
+  } else {
+    login_no_auth_with_email($spendKey, $email);
+  }
 }
+// Not logged in
 if (!logged_in() && !isset($_POST['spendKey'])) {
   echo "Please log in to access Bittorium web wallet!<br><br>";
   echo "<form action='index.php' method='post'>";
